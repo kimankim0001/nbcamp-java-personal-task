94ce397d68f98dc3008351eab296f1770b06a37b
Robbie <robbie331@>
2024-04-30 15:51:08 +0900

    Level1 - 7 연산 결과가 10개로 고정되지 않고 무한이 저장, 배열에서 컬렉션으로 변경되면서 변경해야 하는 부분에 많은 시간을 할애했습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        int[] results = new int[10];
-        // 배열생성
-        int index = 0;
-        // index 를 저장하는 변수를 선언
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        // List 컬렉션 타입의 변수 선언
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("첫 번째 숫자를 입력하세요: ");
@@ -31,20 +29,17 @@ public class App {
                 System.out.println("연산자 기호를 정확히 입력해주세요(+ - * /)");
             }
             System.out.println("결과 " + result);
-            // if 문을 활용하여 index 가 10번째에 다달았을 때,
-            // newResults 배열을 생성하여 results 의 값을 한칸씩 당겨 넣어준다.
-            // 마지막 10번째 값을 newResults 마지막에 저장한다.
-            // else 로 이외의 값을 출력하게 한다.
-            if (index >= results.length) {
-                int[] newResults = new int[10];
-                for (int i = 0; i < 10; i++) {
-                    newResults[i] = results[i + 1];
-                }
-                newResults[9] = result;
-            } else {
-                results[index] = result;
+
+            intList.add(result);
+            // 결과값을 intList 에 저장
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String input = sc.next();
+            if (input.equals("remove")) {
+                intList.remove(0);
             }
-            index++;
+            // remove 를 사용하여 첫 번째 값 삭제
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String end = sc.next();
             if (end.equals("exit")) {
