a19f46903bc5ce20b9e824f746406c56ed2ff407
Robbie <robbie331@>
2024-05-02 16:56:33 +0900

    2-2 calculator 클래스 인스턴스를 생성해주고, 기능들이 수행되도록 코드 수정하였습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator(); // 인스턴스 생성
         Scanner sc = new Scanner(System.in); // Scanner 클래스를 반드시 import
-        List<Integer> results = new ArrayList<>(); // List 인터페이스에 ArrayList 구현체 자료구조를 사용
         do { // 무조건 최초에 한번은 실행되어야 하므로 do while 반복문을 사용
             System.out.println("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num1에 저장
@@ -13,38 +13,16 @@ public class App {
             int num2 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num2에 저장
             System.out.println("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0); // charAt() 을 사용하여 타입을 일치
-            int result = 0; // 연산 결과 값을 저장할 변수 result 를 선언
-            switch (operator) { // if 문 보다 적절하게 표현할 수 있는 switch 문 사용하여 연산
-                case '+' :
-                    result = num1 + num2;
-                    break; // switch 문 사용시 case 다음에는 반드시 break 를 넣어준다
-                case '-' :
-                    result = num1 - num2;
-                    break;
-                case '*' :
-                    result = num1 * num2;
-                    break;
-                case '/' :
-                    if (num2 != 0) { // if 문을 사용하여 분모가 0이 아닐 때에는 연산진행
-                        result = num1 / num2;
-                    } else { // 분모가 0일 때에는 오류에 대한 내용 출력 및 return 으로 종료
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
-                        return;
-                    }
-                    break;
-                default : // 사칙연산 기호 이외의 값 입력 시 오류에 대한 내용 출력 및 return 으로 종료
-                    System.out.println("올바른 선택이 아닙니다.");
-                    return;
-            }
+            int result = calculator.calculate(num1, num2, operator); //Calculator 클래스 연산 결과를 변수에 선언
             System.out.println("결과 : " + result);
-            results.add(result); // .add 메서드를 사용하여 컬렉션에 연산 결과값을 저장
+            calculator.results.add(result); // .add 메서드를 사용하여 컬렉션에 연산 결과값을 저장
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             if ("remove".equals(sc.next())) { // 피드백 내용 반영, 입력값이 remove 와 동일할 때,
-                results.remove(0); // index 0 값을 삭제
+                calculator.results.remove(0); // index 0 값을 삭제
             }
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             if ("inquiry".equals(sc.next())) { // 피드백 내용 반영, 입력값이 inquiry 와 동일할 때,
-                for (Integer res : results) { // 향상된 for 문 사용
+                for (Integer res : calculator.results) { // 향상된 for 문 사용
                     System.out.println("연산결과 : " + res);
                 }
             }
