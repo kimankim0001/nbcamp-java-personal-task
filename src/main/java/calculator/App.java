3a0599e3ffd9184b04d3d347ad58355f60312ba6
Robbie <robbie331@>
2024-05-02 14:48:02 +0900

    1-1 Scanner 를 사용하여 입력받는 값을 변수 num1 과 num2 에 저장하도록 하였습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,15 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner 클래스를 반드시 import
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num1에 저장
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num2에 저장
+
+    }
+
 }
