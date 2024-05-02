f99aee5636ebcaa18700cbfb4c78f4f1cbe8a5ba
Robbie <robbie331@>
2024-05-02 14:25:52 +0900

    reset 피드백 해주신 내용과 과제 해설 영상을 반영하여 처음부터 다시해보면서 이해한 내용을 정리해보겠습니다.

--- src/main/java/calculator/BadException.java
+++ src/main/java/calculator/BadException.java
@@ -1,8 +1,4 @@
 package calculator;
 
-// 예외 클래스를 만들어서 예외를 정의
-public class BadException extends Exception {
-    public BadException() {
-        super("예외 처리");
-    }
+public class BadException {
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,48 +1,4 @@
 package calculator;
 
-import java.util.ArrayList;
-
 public class Calculator {
-
-    private ArrayList<Integer> intList = new ArrayList<Integer>();
-    // 컬렉션 타입 필드를 외부에서 직접 전근하지 못하도록 private 으로 수정
-
-
-    public int calculate(char operator, int num1, int num2) throws BadException{
-        // 예외 플래그를 달아준다.
-        int answer = 0;
-        if (operator == '+') {
-            answer = num1 + num2;
-        } else if (operator == '-') {
-            answer = num1 - num2;
-        } else if (operator == '*') {
-            answer = num1 * num2;
-        } else if (operator == '/') {
-            answer = num1 / num2;
-        } else if (operator == '/' & num2 == 0) { // 나누기에서 분모가 0일때 예외처리
-            throw new BadException();
-        } else { // 연산자에 사칙 연산 기호 이외의 값이 들어갈 때 예외처리
-            throw new BadException();
-        }
-        intList.add(answer);
-        return answer;
-    }
-    // 가장 먼저 저장된 데이터 삭제 기능 가진 removeResult 메서드 구현
-    public void removeResult() {
-        intList.remove(0);
-    }
-    // 저장된 연산 결과들을 조회하는 기능을 가진 inquiryResult 메서드 구현
-    public void inquiryResult() {
-        for (int i : intList) {
-            System.out.println(i + " ");
-        }
-    }
-    // Getter 메서드
-    public ArrayList<Integer> getIntList() {
-        return intList;
-    }
-    // Setter 메서드
-    public void setIntList(ArrayList<Integer> intList) {
-        this.intList = intList;
-    }
 }
