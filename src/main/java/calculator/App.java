8afc61671ab7072ddd265557f351fa959eedd410
Robbie <robbie331@>
2024-05-02 15:04:15 +0900

    1-3 switch 문을 활용하여 사칙 연산 수행, 조건에 해당되는 오류 발생시, 오류에 대한 내용 출력과 return 으로 연산 종료.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,7 +11,30 @@ public class App {
         int num2 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num2에 저장
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0); // charAt() 을 사용하여 타입을 일치
-
+        int result = 0; // 연산 결과 값을 저장할 변수 result 를 선언
+        switch (operator) { // if 문 보다 적절하게 표현할 수 있는 switch 문 사용하여 연산
+            case '+' :
+                result = num1 + num2;
+                break; // switch 문 사용시 case 다음에는 반드시 break 를 넣어준다
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if (num2 != 0) { // if 문을 사용하여 분모가 0이 아닐 때에는 연산진행
+                    result = num1 / num2;
+                } else { // 분모가 0일 때에는 오류에 대한 내용 출력 및 return 으로 종료
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                    return;
+                }
+                break;
+            default : // 사칙연산 기호 이외의 값 입력 시 오류에 대한 내용 출력 및 return 으로 종료
+                System.out.println("올바른 선택이 아닙니다.");
+                return;
+        }
+        System.out.println("결과 : " + result);
     }
 
 }
