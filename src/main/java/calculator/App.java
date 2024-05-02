7c395e7397b83633a5b5169170b314a2c9c6e666
Robbie <robbie331@>
2024-05-02 14:51:56 +0900

    1-2 Scanner 를 사용하여 입력받는 값을 harAt() 을 사용하여 타입을 일치시키고 변수 char 에 저장하도록 하였습니다.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,6 +9,8 @@ public class App {
         int num1 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num1에 저장
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt(); // int 값을 입력받으면 해당 값을 변수 num2에 저장
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0); // charAt() 을 사용하여 타입을 일치
 
     }
 
